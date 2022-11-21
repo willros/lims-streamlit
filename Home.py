d57b7519e7bd675382fe4f10969c43b74b91bcd9
Williams UMU-MacBook <william.rosenbaum@umu.se>
2022-11-21 15:17:49 +0100

    multipage

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Nanopore Telomere Laboration Notebook")
+
+st.sidebar.success("Select what you want to do above.")
+
+st.markdown(
+    """
+    This Laboration notebook let you add information about the sequencing:
+    ### Click on the "Add New Experiment tab above"
+    - Adds new experiment to the database
+    ### View Old Experiments
+    - Look at old experiments
+"""
+)
